111e3e1040de7df425393f8ee8ff916c5a0ddf49
germanapriscila <priscilafortaleza@gmail.com>
2024-08-12 09:41:55 -0300

    Finish project

---
+++ 14_api_restful_com_ts/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "config";
+import Logger from "./logger";
+
+async function connect() {
+    const dbUri = config.get<string>("dbUri");
+
+    try {
+        await mongoose.connect(dbUri);
+        Logger.info("Aplicação conectou ao banco de dados");
+    } catch (e) {
+        Logger.error("Não foi possível conectar ao banco de dados");
+        Logger.error(`Erro: ${e}`);
+        process.exit(1);
+    }
+}
+
+export default connect;
--- 14_api_restful_com_ts/config/default.ts
+++ 14_api_restful_com_ts/config/default.ts
@@ -1,3 +1,8 @@
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
 export default {
-	port: 3000
+	port: 3000,
+	dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qsi63vz.mongodb.net/myMovies?retryWrites=true&w=majority&appName=Cluster0`,
+	env: "development"
 }
